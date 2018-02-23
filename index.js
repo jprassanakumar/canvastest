834e35d5f985c63c8658c63e392d91e004099e09
Jakka Prasanna <prasanna@lyceuminc.com>
2018-02-23 16:50:19 +0530

    logging

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
 
-	var shared = "YOUR SHARED KEY";
+	var shared = "5086c922aa1b7218444b8e5ee0f2e8b818112bce";
   // Grab signed request
   var signed_req = req.body.signed_request;
   // split request at '.'
@@ -38,6 +38,24 @@ app.post('/', function (req, res) {
 
 
   res.sendFile(path.join(views, 'index.html'));
+
+
+  var signedRequest = req.body.signed_request;
+    // Base64-decode the second part of the signed request.
+    var decodedRequest = Desk.canvas.decode(signedRequest.split('.')[1]);
+    // The result is a JSON representation of the context. Let's parse that into a Javascript object.
+    var contextObj = JSON.parse(decodedRequest);
+    // Let's show what we actually get back.
+	console.log(signedRequest);
+    var contextElem = Desk.canvas.byId('context');
+    var greetingElem = Desk.canvas.byId('greeting');
+    greetingElem.innerHTML = 'Hi ' + contextObj.context.user.fullName + '!';
+    // Using JSON.stringify here because this will look much nicer than just showing `decodedRequest`.
+    contextElem.innerHTML = JSON.stringify(contextObj, undefined, 2);
+    // We can also store this so we can reference it later without having to use a global variable.
+    Desk.canvas.client.signedrequest(contextObj);
+
+
 });
 
 var port = process.env.PORT || 9000;
